cea061343669f3f3c1903f1b74e7f87ea70d5d3b
Flavia Muntean <muntean.flavia@gmail.com>
2019-11-22 10:55:36 +0000

    simple bar implemented

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,6 @@
 import "bootstrap";
+import 'simplebar'; // or "import SimpleBar from 'simplebar';" if you want to use it manually.
+import 'simplebar/dist/simplebar.css';
 
 import {copyClipboard} from "../components/clipboard.js";
 
