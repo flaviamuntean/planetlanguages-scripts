b7c5eeb25aea997ca59a752e5fa42c56103a1dbe
Flavia Muntean <muntean.flavia@gmail.com>
2019-11-30 13:38:43 +0000

    instant ajax filters implemented

--- app/models/macro.rb
+++ app/models/macro.rb
@@ -1,5 +1,5 @@
 class Macro < ApplicationRecord
-  validates :name, :description, :code, :tag_list, presence: true
+  validates :name, :description, :code, :tag_list, :file_type_list, presence: true
   has_many :comments, dependent: :destroy
 
   include PgSearch::Model
@@ -8,16 +8,30 @@ class Macro < ApplicationRecord
 
   pg_search_scope :global_search,
     against: [:name, :description, :code],
+    associated_against: {
+      tags: [:name],
+      file_types: [:name],
+      clients: [:name]
+    },
     using: {
       tsearch: { prefix: true }
     }
   acts_as_taggable_on :tags
+  acts_as_taggable_on :file_types
+  acts_as_taggable_on :clients
 
   acts_as_favoritable
 
   def self.tags
-    ['translation', 'approval', 'memoQ', 'single file', 'multiple files', 'Optik',
-     'other', 'Retail Academy', 'Word', 'Excel', 'PowerPoint', 'PDF',
-     'word/char count', 'Swarovski.com', 'TM analysis', 'checking', 'eLearning', 'Axel Springer'].sort_by(&:downcase)
+    ['translation', 'approval', 'memoQ', 'single file', 'multiple files',
+     'other', 'word/char count', 'TM analysis', 'checking', 'eLearning'].sort_by(&:downcase)
+  end
+
+  def self.file_types
+    ['Word', 'Excel', 'PowerPoint', 'PDF'].sort_by(&:downcase)
+  end
+
+  def self.clients
+    ['Optik', 'Retail Academy', 'Swarovski.com', 'Axel Springer'].sort_by(&:downcase)
   end
 end
